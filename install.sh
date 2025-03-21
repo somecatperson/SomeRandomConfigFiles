79c24630b29a22e4924aa759562d85d345cf2733
somecatperson <conneldnnc@gmail.com>
2025-03-21 12:17:29 -0400

    sixth commit

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ mv -i configuration.nix /etc/nixos
 mv -i rofi ~/.config
 mv -i waybar ~/.config
 mv -i hypr ~/.config
-
+chown root:root /etc/nixos/configuration.nix
+chmod 700 /etc/nixos/configuration.nix
+priv=$(ls -l /etc/nixos/configuration.nix)
+echo $priv
+echo 'You should see "-rwx------ 1 root root," if not, that is not correct'
 echo delete this directory
